454f9c613df825cac6e5f11b2d2b4349c9fb5206
EL-BOURISSI Hamza <hamza.elbourissi.10@gmail.com>
2022-05-03 14:16:49 +0000

    CleanCodePfe
    CleanCodePfe

--- app/Http/Controllers/Pfes/PfeController.php
+++ app/Http/Controllers/Pfes/PfeController.php
@@ -3,67 +3,63 @@
 namespace App\Http\Controllers\Pfes;
 
 use App\Interfaces\IPfe;
-use App\Models\Pfa;
-use App\Models\Pfe;
-use App\Models\RapportPfe;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Storage;
 
 
 class PfeController extends Controller
 {
     protected $pfes;
 
-    public function __construct(IPfe $pfes){
+    public function __construct(IPfe $pfes)
+    {
         $this->pfes = $pfes;
     }
 
 
-
-  public function index()
-  {
+    public function index()
+    {
         return $this->pfes->index();
-  }
+    }
 
 
-  public function create()
-  {
+    public function create()
+    {
 
-  }
+    }
 
-  public function store(Request $request)
-  {
-      return $this->pfes->store($request);
-  }
-
-
-  public function show($id)
-  {
-     return $this->pfes->show($id);
-  }
+    public function store(Request $request)
+    {
+        return $this->pfes->store($request);
+    }
 
 
-  public function edit($id)
-  {
+    public function show($id)
+    {
+        return $this->pfes->show($id);
+    }
 
-  }
 
-  public function update(Request $request)
-  {
-      return $this->pfes->update($request);
-  }
+    public function edit($id)
+    {
 
-  public function destroy(Request $request)
-  {
-      return $this->pfes->destroy($request);
-  }
+    }
 
-  public function DownloadFile($titre,$filename){
+    public function update(Request $request)
+    {
+        return $this->pfes->update($request);
+    }
 
-      return $this->pfes->DownloadFile($titre,$filename);
-  }
+    public function destroy(Request $request)
+    {
+        return $this->pfes->destroy($request);
+    }
+
+    public function DownloadFile($titre, $filename)
+    {
+
+        return $this->pfes->DownloadFile($titre, $filename);
+    }
 
 }
 
